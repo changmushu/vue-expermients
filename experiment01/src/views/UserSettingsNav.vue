93063ba4a3e476ea409ecd6c3f0a9d027bc35b6f
changmushu <1173727758@qq.com>
2023-05-29 16:13:15 +0800

    expermient01 update

---
+++ experiment01/src/views/UserSettingsNav.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="us__nav">
+      <router-link to="/emails">emails</router-link>
+      <br />
+      <router-link to="/profile">profile</router-link>
+    </div>
+  </template>
